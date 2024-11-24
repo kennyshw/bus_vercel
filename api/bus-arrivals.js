a37512b78c6a26bd4af40ab3966a13fdc688fb8b
Shin Han Wei, Kenny <112485205+kennyshw@users.noreply.github.com>
2024-11-24 18:42:26 +0800

    Update and rename index.js to bus-arrivals.js

--- api/bus-arrivals.js
+++ api/bus-arrivals.js
@@ -30,12 +30,13 @@ const config = {
     }
 };
 
-export default async function handler(req, res) {
+module.exports = async (req, res) => {
     // Set CORS headers
+    res.setHeader('Access-Control-Allow-Credentials', true);
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS');
-    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-    
+    res.setHeader('Access-Control-Allow-Headers', 'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version');
+
     // Handle OPTIONS request
     if (req.method === 'OPTIONS') {
         res.status(200).end();
